46dd225bb67bf01a860830f9be2768ae40b3a767
JaelynSng <jaelyn.sng@gmail.com>
2021-02-21 18:08:42 +0800

    added index action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
 
 puts 'Creating flats'
 4.times do

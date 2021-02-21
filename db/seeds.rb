9614a0f3f2f415f93032d437990c9f9e32ed801d
JaelynSng <jaelyn.sng@gmail.com>
2021-02-21 17:54:25 +0800

    added model and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating flats'
+4.times do
+  flat = Flat.create!(
+    name: "#{Faker::Dessert.flavor} #{Faker::Dessert.variety}",
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: rand(20..120),
+    number_of_guests: rand(1..6)
+  )
+  puts "Created #{flat.name}"
+end
+puts 'Finished!'
